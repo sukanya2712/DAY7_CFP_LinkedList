c70b80a9b47b20f6af5d1c1dc3b40e2e7c380f2b
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-26 23:15:44 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedLIst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList1 = new LinkedList();
+
+           
+        }
+    }
+}
